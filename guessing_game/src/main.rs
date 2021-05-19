8f311857cab14dc8d9d23dd3fbe8d020b01532ed
Thomas Jung <thomas.jung@sap.com>
2021-05-19 18:00:27 -0400

    generics

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,7 +15,6 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -38,13 +37,15 @@ fn main() {
 pub struct Guess {
     value: i32,
 }
-
 impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}. Are you stupid? 😠", value);
+            panic!(
+                "Guess value must be between 1 and 100, got {}. Are you stupid? 😠",
+                value
+            );
         }
-        Guess {value}
+        Guess { value }
     }
     pub fn value(&self) -> i32 {
         self.value
